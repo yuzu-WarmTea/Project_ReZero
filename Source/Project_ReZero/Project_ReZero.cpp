ec706f84586fdafb41b55144568d1d4ef294e516
Yousif <yz6075@york.ac.uk>
2024-12-05 20:02:17 +0000

    Initial Project Setup

---
+++ Source/Project_ReZero/Project_ReZero.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_ReZero.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_ReZero, "Project_ReZero" );
